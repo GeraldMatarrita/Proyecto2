ab23730a050637c77db9d3d173c861ebf14fb02c
GeraldMatarrita <geramatarr@gmail.com>
2023-04-27 22:19:43 -0600

    OwnerRepository.java eliminado

--- src/main/java/tec/lenguajes/proyecto2/repository/OwnerRepository.java
+++
@@ -1,9 +0,0 @@
-package tec.lenguajes.proyecto2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tec.lenguajes.proyecto2.model.Owner;
-
-public interface OwnerRepository extends JpaRepository<Owner, Integer> {
-
-    Owner findFirstById(Integer id);
-}
