639b99241365552e6365be08396e073a588b9be6
GeraldMatarrita <geramatarr@gmail.com>
2023-05-01 23:05:44 -0600

    Update

--- src/main/java/tec/lenguajes/proyecto2/repository/ImageRepository.java
+++ src/main/java/tec/lenguajes/proyecto2/repository/ImageRepository.java
@@ -3,9 +3,9 @@ package tec.lenguajes.proyecto2.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tec.lenguajes.proyecto2.model.Image;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ImageRepository extends JpaRepository<Image, Integer> {
-
     Optional<Image> findFirstById(Integer id);
 }
